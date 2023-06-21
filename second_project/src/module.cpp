6c1ab5fda0669efd1c8b6cc507b28cf15d978627
Comodaino <121240084+Comodaino@users.noreply.github.com>
2023-06-21 11:28:03 +0200

    Add files via upload

--- second_project/src/module.cpp
+++ second_project/src/module.cpp
@@ -1,8 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "first_project/Odom.h"
 #include <nav_msgs/Odometry.h>
-#include "first_project/ResetOdom.h"
 #include <geometry_msgs/Quaternion.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
@@ -10,9 +8,6 @@
 #include <math.h>
 
 #include <sstream>
-
-#define D 2.8
-#define PI 3.14
 
 class OdomNode {
 private:
@@ -27,43 +22,25 @@ public:
 
     OdomNode() {
         odometry_sub = n.subscribe("t265/odom", 1, &OdomNode::odom_callback, this);
-        laser_scan_sub = n.subscribe("/scan", 1, &OdomNode::laser_callback, this);
-        lidar_scan_sub = n.subscribe("/converted_scan", 1, &OdomNode::lidar_callback, this);
-        stamp = ros::Time::now();
-        ROS_INFO("CREATED NODE");
-        ROS_INFO("x: %f, y: %f", this->x, this->y);
     }
 
-    void odom_callback(const geometry_msgs::Quaternion::ConstPtr &quat) {
-        quaternion = *quat;
-        publishTf();
-    }
+    void odom_callback(const nav_msgs::Odometry::ConstPtr &odom_msg) {
 
-    void laser_callback(const velodyne_points)
+        nav_msgs::Odometry msg = *odom_msg;
 
-    void publishTf() {
         tf::Transform transform;
-        transform.setOrigin(tf::Vector3(this->x, this->y, 0));
-        tf::Quaternion q;
-        transform.setRotation(tf::createQuaternionFromYaw(this->th));
-        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "base_link"));
-    }
 
 
-    void laser_callback(const turtlesim::PoseConstPtr &msg) {
-        tf::Transform transform;
-        transform.setOrigin(tf::Vector3(msg->x, msg->y, 0.0));
-        tf::Quaternion q;
-        q.setRPY(0, 0, msg->theta);
-        transform.setRotation(q);
-        this.br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "base_link"));
+        transform.setOrigin(tf::Vector3(msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z));
+        transform.setRotation(tf::createQuaternionFromYaw(msg.pose.pose.orientation.w));
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "odom", "t265"));
     }
 
 };
 
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "second_project");
+    ros::init(argc, argv, "tf_publisher");
 
     OdomNode node;
 
